d4814d5e5e7f942544e18445d78c8d39b8d2e790
hariharandevarakonda <dshariharan99@gmail.com>
2024-09-18 13:43:39 +0000

    datatypes

---
+++ JS-basics/data-types.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3)// we are using nodejs, not browser
+
+console.log(3 + 3); //code readability should be high
+
+console.log("hariharan");
+
+
+let name = "HARI"
+let  age = 25
+let isLoggedIn = true
+
+
+/*number => 2 to the power 53
+ bigint=> used for larger number
+ string=> '' / ""
+ boolean=> true/false
+ null=>standalone value
+ undefined=>no value is defined 
+ symbol=>unique
+ */
+
+ //objects
+
+
+ console.log(typeof(age))
+
+ console.log(typeof null);//object
+ console.log(typeof undefined);//undefined
+ 
+ 
