f03250e64ebe2c5bd85d581bdd939dfb5dea5a6f
hariharandevarakonda <dshariharan99@gmail.com>
2024-09-21 09:37:06 +0000

    updated

--- JS-basics/convertion-operation.js
+++ JS-basics/convertion-operation.js
@@ -4,4 +4,11 @@ console.log(score)
 console.log(typeof score);
 
 let isNumber = Number(score)
-console.log(typeof isNumber)
+// console.log(typeof isNumber)
+
+// *********************** operations *********************
+
+let num1 = 50
+num1++;  // prefix ++a / postfix a++
+console.log(num1);
+
