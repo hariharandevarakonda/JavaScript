bc886b0cd649f860d2f72fde2585308d7a013dad
hariharandevarakonda <dshariharan99@gmail.com>
2024-11-06 10:32:05 +0000

    two

---
+++ 05-iterations.js/two.js
@@ -0,0 +1,25 @@
+// while and do while
+
+let index =0
+while (index<=10) {
+    // console.log(`value of index is ${index}`);
+    index =index + 3
+}
+
+
+let myArray = ["flash", 223, 'batman', 18, 'NULL']
+let arr = 0
+
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+// do while
+
+let score = 9
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 8);
