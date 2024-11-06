e5b9b37fff824e85ab5e16b8108440eeea3590db
hariharandevarakonda <dshariharan99@gmail.com>
2024-11-06 07:48:27 +0000

    loop

---
+++ 05-iterations.js/one.js
@@ -0,0 +1,43 @@
+// // for
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is the middle number");
+        
+//     }
+//     // console.log(element);
+    
+// }
+
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outer loop value ${i}`);
+    
+//     for (let j = 0; j <=10; j++) {        
+//         // console.log(`inner loop value ${j} and inner loop ${i}`);
+//       console.log(i + '*' + j + '=' + i*j);
+        
+//     }
+    
+// }
+
+let myArr = [12345, "Batman", 3.145, '3+9k']
+// console.log(myArr.length);
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    // console.log(element);
+    
+}
+
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log(`Detected 5`);
+        // break
+        // continue
+    }
+    console.log(`value of i is ${index}`);
+}
